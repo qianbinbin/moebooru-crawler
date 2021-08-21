e803d58fa953de5b6518659e3238053dd4acfaff
Binbin Qian <qianbinbin@hotmail.com>
2021-08-21 23:23:51 +0800

    Remove download #3 #4

--- moebooru-crawler.sh
+++ moebooru-crawler.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-DIR=
 NUM=0
-URLS_ONLY=false
-MAX_PROCS=8
 
 
 ################################################################################
 # PARSE ARGS
 
 usage() {
-  echo "Usage: moebooru-crawler URL [ -d | --dir DIR ]
-                            [ -n | --num NUM ]
-                            [ -u | --urls-only ]
-                            [ -p | --max-procs PROCS ]"
+  echo "Usage: moebooru-crawler URL [ -n | --num NUM ]"
   exit 2
 }
 
-PARSED_ARGUMENTS=$(getopt -a -n moebooru-crawler -o d:n:up: --long dir:,num:,urls-only,max-procs: -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n moebooru-crawler -o n: --long num: -- "$@")
 
 VALID_ARGUMENTS=$?
 [ "$VALID_ARGUMENTS" != 0 ] && usage
@@ -25,10 +19,7 @@ VALID_ARGUMENTS=$?
 eval set -- "$PARSED_ARGUMENTS"
 while :; do
   case "$1" in
-    -d | --dir)        DIR="$2"         ; shift 2 ;;
     -n | --num)        NUM="$2"         ; shift 2 ;;
-    -u | --urls-only)  URLS_ONLY=true   ; shift   ;;
-    -p | --max-procs)  MAX_PROCS="$2"   ; shift 2 ;;
     --) shift; break ;;
   esac
 done
@@ -73,22 +64,4 @@ else
   links=$(head -n "$NUM" <<< "$links")
 fi
 
-[ "$URLS_ONLY" = true ] && echo "$links" && exit 0
-
-
-################################################################################
-# DOWNLOAD FILES
-
-if [ "$DIR" ] ; then
-  [ ! -d "$DIR" ] && mkdir -p "$DIR"
-  cd  "$DIR" || exit 1
-fi
-
-dir=$(sed "s/[^A-Za-z0-9\._-]/_/g" <<< "${URL#*://}")
-[ "$NUM" -gt 0 ] && dir+="-$NUM"
-
-[ ! -d "$dir" ] && mkdir "$dir"
-cd "$dir" || exit 1
-
-# do not use buggy curl --parallel
-echo "$links" | xargs -n 1 -P "$MAX_PROCS" -I {} bash -c 'curl -fsSL --retry 4 -O "{}" || echo "failed to download: {}" 1>&2'
+echo "$links"
